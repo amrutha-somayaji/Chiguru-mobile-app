2b9f5ce1870357ebdf9f0654148d78e75a500f32
Ritu Deshpande <ritudpande@gmail.com>
2020-03-28 20:56:42 +0530

    added QR code scanning fuctionality to floating action button by creating a new Scnner activity

---
+++ app/src/main/java/com/chiguruecospace/chiguru_mobile_app/Scanner.java
@@ -0,0 +1,66 @@
+package com.chiguruecospace.chiguru_mobile_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class Scanner extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+    private TextView txtResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+        //init
+        scannerView = (ZXingScannerView)findViewById(R.id.zxscan);
+        txtResult = (TextView)findViewById(R.id.txt_result);
+
+        //request permission
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        scannerView.setResultHandler(Scanner.this);
+                        scannerView.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(Scanner.this, "You must grant this permission to scan the code", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                })
+                .check();
+    }
+
+    @Override
+    protected void onDestroy() {
+        scannerView.stopCamera();
+        super.onDestroy();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        //Here we can receive rawResult
+        txtResult.setText(rawResult.getText());
+    }
+}
