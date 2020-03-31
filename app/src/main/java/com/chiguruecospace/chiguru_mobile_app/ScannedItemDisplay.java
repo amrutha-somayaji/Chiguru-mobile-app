1f2202db0272a6710e2e17b261b073f0a05b6fe2
captainspockears <ruthuparna1998@gmail.com>
2020-03-31 19:13:56 +0530

    Added image support to ScannedItemDisplay

--- app/src/main/java/com/chiguruecospace/chiguru_mobile_app/ScannedItemDisplay.java
+++ app/src/main/java/com/chiguruecospace/chiguru_mobile_app/ScannedItemDisplay.java
@@ -1,7 +1,10 @@
 package com.chiguruecospace.chiguru_mobile_app;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,17 +16,25 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ScannedItemDisplay extends AppCompatActivity {
 
     private static final String TAG = "ScannedItemDisplay";
     private static final String KEY_TITLE = "Name";
     private static final String KEY_DESCRIPTION = "Description";
+    private static final String KEY_IMAGE_PATH = "Image-path";
     private TextView textViewData;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private String QRRESULT = Scanner.getQRdata();
     private String path = "items/"+QRRESULT;
     private DocumentReference noteRef = db.document(path);
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,7 @@ public class ScannedItemDisplay extends AppCompatActivity {
     }
 
     public void loadNote() {
+
         noteRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
@@ -41,8 +53,31 @@ public class ScannedItemDisplay extends AppCompatActivity {
                         if (documentSnapshot.exists()) {
                             String name = documentSnapshot.getString(KEY_TITLE);
                             String description = documentSnapshot.getString(KEY_DESCRIPTION);
+                            String imagepath = documentSnapshot.getString(KEY_IMAGE_PATH);
 
-                            //Map<String, Object> note = documentSnapshot.getData();
+                            FirebaseStorage storage = FirebaseStorage.getInstance();
+                            // Create a reference to a file from a Google Cloud Storage URI
+                            StorageReference gsReference = storage.getReferenceFromUrl("gs://chiguru-mobile-app.appspot.com/items-pictures/"+imagepath);
+
+                            image = (ImageView) findViewById(R.id.imageView2);
+
+                            try {
+                                final File file = File.createTempFile("image","jpg");
+                                gsReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                                        image.setImageBitmap(bitmap);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(ScannedItemDisplay.this, "Image failed to load", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
 
                             String text = "Name: " + name + "\n" + "Description: " + description;
                             textViewData.setText(text);
