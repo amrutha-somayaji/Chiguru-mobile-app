733e569c0864a2233bfd059e7d4c803b4cc8b933
captainspockears <ruthuparna1998@gmail.com>
2020-04-25 20:44:06 +0530

    Added about us

--- app/src/main/java/com/chiguruecospace/chiguru_mobile_app/ui/aboutus/AboutusFragment.java
+++ app/src/main/java/com/chiguruecospace/chiguru_mobile_app/ui/aboutus/AboutusFragment.java
@@ -1,9 +1,13 @@
 package com.chiguruecospace.chiguru_mobile_app.ui.aboutus;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,10 +18,30 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.chiguruecospace.chiguru_mobile_app.R;
 import com.chiguruecospace.chiguru_mobile_app.ui.aboutus.AboutusModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
+import io.grpc.okhttp.internal.Platform;
 
 public class AboutusFragment extends Fragment {
 
     private AboutusModel aboutusViewModel;
+    private FirebaseFirestore db;
+
+    private ImageView aboutuscoverimage;
+    private TextView aboutusheader;
+    private TextView aboutusdesc;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +49,58 @@ public class AboutusFragment extends Fragment {
                 ViewModelProviders.of(this).get(AboutusModel.class);
         View root = inflater.inflate(R.layout.fragment_aboutus, container, false);
 
+        db = FirebaseFirestore.getInstance();
+
+        aboutuscoverimage = root.findViewById(R.id.aboutusimage);
+        aboutusheader = root.findViewById(R.id.aboutusheadingtext);
+        aboutusdesc = root.findViewById(R.id.aboutusdesctext);
+
+        DocumentReference docRef = db.collection("chiguruinfo").document("aboutus");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("Username", "DocumentSnapshot data: " + document.getData());
+                        final String imagepath = document.getString("Imagepath");
+                        final String title = document.getString("Title");
+                        final String desc = document.getString("Description");
+
+                        aboutusheader.setText(title);
+                        aboutusdesc.setText(desc);
+
+                        FirebaseStorage storage = FirebaseStorage.getInstance();
+                        // Create a reference to a file from a Google Cloud Storage URI
+                        StorageReference gsReference = storage.getReferenceFromUrl(imagepath);
+
+                        try {
+                            final File file = File.createTempFile("image","jpg");
+                            gsReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                    Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                                    aboutuscoverimage.setImageBitmap(bitmap);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    } else {
+                        Log.d("Username", "No such document");
+                    }
+                } else {
+                    Log.d("Username", "get failed with ", task.getException());
+                }
+            }
+        });
         return root;
     }
 }
